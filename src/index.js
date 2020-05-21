db3ac43d15fa94be6aa0dc42ab599c97b001f15c
arielnt1993 <61444560+arielnt1993@users.noreply.github.com>
2020-05-21 19:57:02 -0300

    clase 20/05/2020

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+// imported modules
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+app.use(morgan('dev'));
+
+app.get('/',(req,res)=>{
+    res.send('hola mundo');
+});
 
 app.listen(3000,()=>{
     console.log(`Server listen on port ${3000}`);
