f16b0f6ef9e104b02ccf37d374e5154b18103ecc
arielnt1993 <61444560+arielnt1993@users.noreply.github.com>
2020-05-22 01:30:34 -0300

    clase 20/05/2020

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,11 +1,32 @@
 //import modules
 const { Router } = require("express");
 const router = Router();
+const _ = require("lodash");
 //import json
 const movies = require("../sample.json");
 
 router.get("/movies", (req, res) => {
   res.json(movies);
+});
+
+router.post("/movies", (req, res) => {
+  const { title, director, year, rating } = req.body;
+  if (title && director && year && rating) {
+    const newMovie = { ...req.body };
+    movies.push(newMovie);
+    res.json({ added: "ok" });
+  } else {
+    res.status(400).json({ statusCode: "badRequest" });
+  }
+});
+
+router.delete("/movies/:id", (req, res) => {
+  const id = req.params.id;
+  _.remove(movies, (movie) => {
+    return movie.id == id;
+  });
+  console.log(req.params);
+  res.json();
 });
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,21 @@
 const express = require("express");
 const morgan = require("morgan");
 const router = require("../routes/index");
-
+const app = express();
 /* asigning express() to app, then tell express that morgan will
    be displayed on dev mode, then tell express to use router as a
    route
 */
-const app = express();
+
+let port = 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
 app.use(morgan("dev"));
 app.use(router);
 
-app.listen(3000, () => {
-  console.log(`Server listen on port ${3000}`);
+app.listen(port, () => {
+  console.log(`Server listen on port ${port}`);
 });
